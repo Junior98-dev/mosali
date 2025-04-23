2f2fae2ac1d940c02a1a9aeb40f940e05e4656dd
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-04-23 23:33:54 +0200

    Ajout component employeeInfo , pour afficher les information d'un employé

---
+++ src/app/ui/home/employee-info/employee-info.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-info',
+  imports: [],
+  template: `
+    <p>
+      employee-info works!
+    </p>
+  `,
+  styles: ``
+})
+export class EmployeeInfoComponent {
+
+}
