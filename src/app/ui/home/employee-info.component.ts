0fea77f758feed34f6076aa1181db3ef5f3cc29f
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-04-24 22:49:25 +0200

    ajout de la variable employee pour recevoir les données du  composant parent employee-list

--- src/app/ui/home/employee-info.component.ts
+++ src/app/ui/home/employee-info.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { employee } from '../../core/models/employee.model';
 
 @Component({
   selector: 'app-employee-info',
   imports: [],
   template: `
-    <p>
-      employee-info works!
-    </p>
+    <h4>{{employee().fullName}}</h4>
+    <h5>{{employee().email}} - +{{employee().phone}} - Sexe: {{employee().sexe}}</h5>
+    <p>Adresse: {{employee().address.street}} - {{employee().address.city}}/{{employee().address.state}}/{{employee().address.country}}</p>
+    @for (hobby of employee().hobbies; track $index) {
+      <p>passe-temps: N°{{$index + 1}} - {{hobby}}</p>
+    }
+    
   `,
   styles: ``
 })
 export class EmployeeInfoComponent {
-
+  employee = input.required<employee>();
 }
