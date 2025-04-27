37dcaf393580a415687e103def8f4205486c8e7a
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-04-27 19:17:28 +0200

    Ajout des méthodes editEmployee et deleteEmployee dans le service EmployeeService pour gérer la modification et la suppression des employés.

--- src/app/core/services/employee.service.ts
+++ src/app/core/services/employee.service.ts
@@ -11,5 +11,7 @@ export class EmployeeService {
   addEmployee = (employee: employee) =>{this.employees.push(employee);}
   getEmployees = () => this.employees;
   getEmployee = (index: number) => this.employees[index]; // find the employee by index
+  editEmployee = (i: number, employee: employee) => {this.employees[i] = employee;}
+  deleteEmployee = (i: number) => {this.employees.splice(i, 1);} // delete the employee by index
 
 }
