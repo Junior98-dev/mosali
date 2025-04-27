3a25ddcb6cc24b762bba3ad9aff23167c0d40d46
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-04-27 19:10:55 +0200

    Mise à jour du composant DetailEmployee pour intégrer le composant EmployeeInfo et gérer la propriété toolbarTitle. Ajout de la logique pour définir le titre de la page en fonction de l'employé sélectionné.

--- src/app/ui/detail-employee/detail-employee.component.ts
+++ src/app/ui/detail-employee/detail-employee.component.ts
@@ -2,12 +2,16 @@ import { Component, inject, input, OnInit } from '@angular/core';
 import { NavigationBackToolbarComponent } from "../shared/navigation-back-toolbar.component";
 import { employee } from '../../core/models/employee.model';
 import { EmployeeService } from '../../core/services/employee.service';
+import { Router} from '@angular/router';
+import { EmployeeInfoComponent } from "../home/employee-info.component";
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-detail-employee',
-  imports: [NavigationBackToolbarComponent],
+  imports: [NavigationBackToolbarComponent, EmployeeInfoComponent],
   template: `
-   <app-navigation-back-toolbar [toolbarTitle]="employee.fullName"/> 
+   <app-navigation-back-toolbar [toolbarTitle]="employee ? employee.fullName : ''"/> 
+   <app-employee-info [employee]="employee"/>
   `,
   styles: ``
 })
@@ -15,11 +19,19 @@ export default class DetailEmployeeComponent implements OnInit {
   id = input();
   employee!:employee;
   private es = inject(EmployeeService);
+  private router = inject(Router);
+  private title = inject(Title);
+
 
   ngOnInit(): void {
     console.log(this.id());
     
     this.employee = this.es.getEmployee(Number(this.id()));
+    if (this.employee) {
+      this.title.setTitle(`${this.employee.fullName} - Mosali`);
+    }else{
+      this.router.navigate(['/']);
+    }
   }
 
 }
